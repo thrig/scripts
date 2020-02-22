e7937a951168869995a38cbb7880fc0c28020372
Jeremy Mates <jeremy.mates@gmail.com>
2020-02-22 19:14:25 +0000

    remove unused server socket from crosstalkc

--- network/crosstalk/crosstalkc.c
+++ network/crosstalk/crosstalkc.c
@@ -16,8 +16,8 @@
 #include <time.h>
 #include <unistd.h>
 
-#include <event2/bufferevent.h>
 #include <event2/buffer.h>
+#include <event2/bufferevent.h>
 #include <event2/event.h>
 
 #define NSEC_IN_SEC 1000000000U
@@ -31,63 +31,51 @@ void readcb(struct bufferevent *bev, void *ptr);
 void *schedule_talk(void *ptr);
 double talk_delay(void);
 
-int main(int argc, char *argv[])
-{
-    char *sock_path;
-    int ch, server;
+int main(int argc, char *argv[]) {
     struct bufferevent *ev_bev;
     struct event *ev_alarm;
     struct event_base *ev_base;
     struct sockaddr_un addr;
 
 #ifdef __OpenBSD__
-    if (pledge("rpath stdio unix unveil", NULL) == -1)
-        err(1, "pledge failed");
+    if (pledge("rpath stdio unix unveil", NULL) == -1) err(1, "pledge failed");
 #endif
 
+    int ch;
     while ((ch = getopt(argc, argv, "h?C:")) != -1) {
         switch (ch) {
         case 'C':
-            if (chdir(optarg) == -1)
-                err(1, "chdir failed");
+            if (chdir(optarg) == -1) err(1, "chdir failed");
             break;
         case 'h':
         case '?':
-        default:
-            emit_help();
+        default: emit_help();
         }
     }
     argc -= optind;
     argv += optind;
 
-    if (argc < 1)
-        emit_help();
+    if (argc < 1) emit_help();
 
     /* this is really low on some systems; use -C dir and a relative
      * socket path if run into this limit? */
     if (strlen(argv[0]) >= sizeof(addr.sun_path))
         errx(EX_DATAERR, "socket name longer than sun_path");
-    sock_path = argv[0];
+    char *sock_path = argv[0];
 
 #ifdef __OpenBSD__
-    if (unveil(argv[0], "r") == -1)
-        err(1, "unveil failed");
-    if (unveil(NULL, NULL) == -1)
-        err(1, "unveil failed");
+    if (unveil(argv[0], "r") == -1) err(1, "unveil failed");
+    if (unveil(NULL, NULL) == -1) err(1, "unveil failed");
 #endif
-
-    if ((server = socket(AF_UNIX, SOCK_STREAM, 0)) == -1)
-        err(EX_IOERR, "socket failed");
 
     memset(&addr, 0, sizeof(addr));
     addr.sun_family = AF_UNIX;
     strncpy(addr.sun_path, sock_path, sizeof(addr.sun_path) - 1);
 
-    if ((ev_base = event_base_new()) == NULL)
-        err(1, "event_base_new failed");
+    if ((ev_base = event_base_new()) == NULL) err(1, "event_base_new failed");
 
-    if ((ev_bev =
-         bufferevent_socket_new(ev_base, -1, BEV_OPT_CLOSE_ON_FREE)) == NULL)
+    if ((ev_bev = bufferevent_socket_new(ev_base, -1, BEV_OPT_CLOSE_ON_FREE)) ==
+        NULL)
         errx(1, "bufferevent_socket_new failed");
 
     bufferevent_setcb(ev_bev, readcb, NULL, eventcb, NULL);
@@ -100,53 +88,48 @@ int main(int argc, char *argv[])
     sigemptyset(&block);
     sigaddset(&block, SIGALRM);
 
-    if (bufferevent_socket_connect
-        (ev_bev, (struct sockaddr *) &addr, sizeof(addr)) == -1)
+    if (bufferevent_socket_connect(ev_bev, (struct sockaddr *) &addr,
+                                   sizeof(addr)) == -1)
         err(EX_IOERR, "bufferevent_socket_connect failed");
 
     bufferevent_enable(ev_bev, EV_READ);
 
 #ifdef __OpenBSD__
-    if (pledge("stdio", NULL) == -1)
-        err(1, "pledge failed");
+    if (pledge("stdio", NULL) == -1) err(1, "pledge failed");
 #endif
 
     event_base_dispatch(ev_base);
     exit(42);
 }
 
-void cross_talk(evutil_socket_t fd, short events, void *ptr)
-{
+void cross_talk(evutil_socket_t fd, short events, void *ptr) {
     int ch;
     struct bufferevent *ev_bev = ptr;
-    struct evbuffer *output = bufferevent_get_output(ev_bev);
-    ch = 65 + arc4random_uniform(26);
+    struct evbuffer *output    = bufferevent_get_output(ev_bev);
+    ch                         = 65 + arc4random_uniform(26);
     evbuffer_add(output, &ch, 1);
 }
 
-void emit_help(void)
-{
+void emit_help(void) {
     fputs("Usage: crosstalkc [-C dir] socket-path\n", stderr);
     exit(EX_USAGE);
 }
 
-void eventcb(struct bufferevent *ev_bev, short events, void *ptr)
-{
+void eventcb(struct bufferevent *ev_bev, short events, void *ptr) {
     if (events & BEV_EVENT_CONNECTED) {
         pthread_t tid;
         if (pthread_create(&tid, NULL, schedule_talk, NULL) != 0)
             err(EX_OSERR, "pthread_create failed");
     } else if (events & (BEV_EVENT_EOF | BEV_EVENT_ERROR)) {
         // may instead want a sleep/restart but that's complicated so just
-        //bufferevent_free(ev_bev);
+        // bufferevent_free(ev_bev);
         exit(1);
     } else if (events & BEV_EVENT_ERROR) {
         warn("bufferevent error");
     }
 }
 
-void readcb(struct bufferevent *ev_bev, void *ptr)
-{
+void readcb(struct bufferevent *ev_bev, void *ptr) {
     char buf[2048];
     int n, total = 0;
     struct timespec delay;
@@ -157,35 +140,32 @@ void readcb(struct bufferevent *ev_bev, void *ptr)
         write(STDOUT_FILENO, buf, n);
     }
     if (total > 4) {
-        delay.tv_sec = 1 + arc4random_uniform(3);
+        delay.tv_sec  = 1 + arc4random_uniform(3);
         delay.tv_nsec = arc4random_uniform(NSEC_IN_SEC);
         nanosleep(&delay, NULL);
     }
     sigprocmask(SIG_UNBLOCK, &block, NULL);
 }
 
-void *schedule_talk(void *ptr)
-{
+void *schedule_talk(void *ptr) {
     double secs, nsecs;
     struct timespec delay;
     while (1) {
         raise(SIGALRM);
-        nsecs = modf(talk_delay(), &secs);
-        delay.tv_sec = secs;
+        nsecs         = modf(talk_delay(), &secs);
+        delay.tv_sec  = secs;
         delay.tv_nsec = NSEC_IN_SEC * nsecs;
         nanosleep(&delay, NULL);
     }
     return (void *) 0;
 }
 
-inline double talk_delay(void)
-{
+inline double talk_delay(void) {
     double after;
-    uint32_t delay = 100000000 +
-        arc4random_uniform(100000000) +
-        arc4random_uniform(100000000) + arc4random_uniform(100000000);
+    uint32_t delay = 100000000 + arc4random_uniform(100000000) +
+                     arc4random_uniform(100000000) +
+                     arc4random_uniform(100000000);
     after = delay / (double) NSEC_IN_SEC;
-    if (arc4random() > 3865470565)
-        after *= 3;
+    if (arc4random() > 3865470565) after *= 3;
     return after;
 }
--- network/crosstalk/crosstalkd.c
+++ network/crosstalk/crosstalkd.c
@@ -40,8 +40,7 @@ void cross_event(struct bufferevent *ev_bev, short events, void *ptr);
 void cross_talk(struct bufferevent *ev_bev, void *ptr);
 void emit_help(void);
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int ch;
     char *sock_path;
     struct evconnlistener *c;
@@ -56,8 +55,7 @@ int main(int argc, char *argv[])
     while ((ch = getopt(argc, argv, "h?C:")) != -1) {
         switch (ch) {
         case 'C':
-            if (chdir(optarg) == -1)
-                err(1, "chdir failed");
+            if (chdir(optarg) == -1) err(1, "chdir failed");
             break;
         case 'h':
         case '?':
@@ -69,8 +67,7 @@ int main(int argc, char *argv[])
     argc -= optind;
     argv += optind;
 
-    if (argc != 1)
-        emit_help();
+    if (argc != 1) emit_help();
 
     /* this is really low on some systems; use -C dir and a relative
      * socket path if run into this limit? */
@@ -79,14 +76,11 @@ int main(int argc, char *argv[])
     sock_path = argv[0];
 
 #ifdef __OpenBSD__
-    if (unveil(argv[0], "crw") == -1)
-        err(1, "unveil failed");
-    if (unveil(NULL, NULL) == -1)
-        err(1, "unveil failed");
+    if (unveil(argv[0], "crw") == -1) err(1, "unveil failed");
+    if (unveil(NULL, NULL) == -1) err(1, "unveil failed");
 #endif
 
-    if ((ev_base = event_base_new()) == NULL)
-        err(1, "event_base_new failed");
+    if ((ev_base = event_base_new()) == NULL) err(1, "event_base_new failed");
 
     for (int i = 0; i < REPLY_BUF_SIZE; i++)
         buf[i] = 97 + arc4random_uniform(26);
@@ -97,19 +91,17 @@ int main(int argc, char *argv[])
 
     unlink(sock_path);
 
-    if ((c = evconnlistener_new_bind(ev_base, accept_cb, NULL,
-                                     LEV_OPT_CLOSE_ON_EXEC |
-                                     LEV_OPT_CLOSE_ON_FREE | LEV_OPT_REUSEABLE,
-                                     -1, (struct sockaddr *) &addr,
-                                     sizeof(addr))) == NULL)
+    if ((c = evconnlistener_new_bind(
+             ev_base, accept_cb, NULL,
+             LEV_OPT_CLOSE_ON_EXEC | LEV_OPT_CLOSE_ON_FREE | LEV_OPT_REUSEABLE,
+             -1, (struct sockaddr *) &addr, sizeof(addr))) == NULL)
         err(1, "could not listen on %s", argv[0]);
     evconnlistener_set_error_cb(c, accept_error_cb);
 
     signal(SIGPIPE, SIG_IGN);
 
 #ifdef __OpenBSD__
-    if (pledge("inet stdio", NULL) == -1)
-        err(1, "pledge failed");
+    if (pledge("inet stdio", NULL) == -1) err(1, "pledge failed");
 #endif
 
 #ifdef SYSTEMD
@@ -121,8 +113,7 @@ int main(int argc, char *argv[])
 }
 
 void accept_cb(struct evconnlistener *c, evutil_socket_t client,
-               struct sockaddr *addr, int len, void *ptr)
-{
+               struct sockaddr *addr, int len, void *ptr) {
     struct event_base *ev_base = evconnlistener_get_base(c);
     struct bufferevent *ev_bev =
         bufferevent_socket_new(ev_base, client, BEV_OPT_CLOSE_ON_FREE);
@@ -130,59 +121,51 @@ void accept_cb(struct evconnlistener *c, evutil_socket_t client,
     bufferevent_enable(ev_bev, EV_READ);
 }
 
-void accept_error_cb(struct evconnlistener *c, void *ptr)
-{
+void accept_error_cb(struct evconnlistener *c, void *ptr) {
     struct event_base *ev_base = evconnlistener_get_base(c);
-    int err = EVUTIL_SOCKET_ERROR();    // NOTE requires errno.h
+    int err                    = EVUTIL_SOCKET_ERROR(); // NOTE requires errno.h
     warnx("listener error (%d): %s", err, evutil_socket_error_to_string(err));
     event_base_loopexit(ev_base, NULL);
 }
 
-void cross_event(struct bufferevent *ev_bev, short events, void *ptr)
-{
-    if (events & (BEV_EVENT_EOF | BEV_EVENT_ERROR))
-        bufferevent_free(ev_bev);
-    if (events & BEV_EVENT_ERROR)
-        warn("bufferevent error");
+void cross_event(struct bufferevent *ev_bev, short events, void *ptr) {
+    if (events & (BEV_EVENT_EOF | BEV_EVENT_ERROR)) bufferevent_free(ev_bev);
+    if (events & BEV_EVENT_ERROR) warn("bufferevent error");
 }
 
-void cross_talk(struct bufferevent *ev_bev, void *ptr)
-{
+void cross_talk(struct bufferevent *ev_bev, void *ptr) {
     size_t len;
-    static int no_echo = 0;
-    static size_t seen = 0;
+    static int no_echo      = 0;
+    static size_t seen      = 0;
     static size_t threshold = 0;
-    //struct evbuffer_iovec v;
-    struct evbuffer *input = bufferevent_get_input(ev_bev);
+    // struct evbuffer_iovec v;
+    struct evbuffer *input  = bufferevent_get_input(ev_bev);
     struct evbuffer *output = bufferevent_get_output(ev_bev);
     uint32_t val;
 
-    //int n = evbuffer_peek(input, -1, NULL, &v, 1);
-    //for (int i = 0; i < n; i++) {
+    // int n = evbuffer_peek(input, -1, NULL, &v, 1);
+    // for (int i = 0; i < n; i++) {
     //    fwrite(v.iov_base, 1, v.iov_len, stderr);
     //}
-    //putc('\n', stderr);
+    // putc('\n', stderr);
 
-    if (threshold == 0)
-        threshold = 2 + arc4random_uniform(30);
+    if (threshold == 0) threshold = 2 + arc4random_uniform(30);
 
     len = evbuffer_get_length(input);
 
-    if ((val = arc4random()) > 4252017622)
-        no_echo = 1 + (val & 15);
+    if ((val = arc4random()) > 4252017622) no_echo = 1 + (val & 15);
     if (no_echo == 0) {
         evbuffer_add_buffer(output, input);
     } else {
-        //putc('N', stderr);
+        // putc('N', stderr);
         evbuffer_drain(input, len);
         no_echo -= len;
-        if (no_echo < 0)
-            no_echo = 0;
+        if (no_echo < 0) no_echo = 0;
     }
     if ((seen += len) > threshold) {
         size_t buflen, start;
-        start = arc4random_uniform(REPLY_BUF_SIZE / 2 - 1);
-        buflen = 1 + arc4random_uniform(REPLY_BUF_SIZE / 2 - 1);
+        start              = arc4random_uniform(REPLY_BUF_SIZE / 2 - 1);
+        buflen             = 1 + arc4random_uniform(REPLY_BUF_SIZE / 2 - 1);
         replydelay.tv_nsec = arc4random_uniform(NSEC_IN_SEC);
         nanosleep(&replydelay, NULL);
         evbuffer_add(output, buf + start, buflen);
@@ -190,8 +173,7 @@ void cross_talk(struct bufferevent *ev_bev, void *ptr)
     }
 }
 
-void emit_help(void)
-{
+void emit_help(void) {
     fputs("Usage: crosstalkd [-C dir] socket-file\n", stderr);
     exit(EX_USAGE);
 }
