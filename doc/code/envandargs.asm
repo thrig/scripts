3d0cbf4d2ce6e21ca3a56a6870e9976809990f1f
Jeremy Mates <jeremy.mates@gmail.com>
2021-06-27 14:23:40 -0700

    rework them code blocks

--- doc/code/envandargs.asm
+++ doc/code/envandargs.asm
@@ -5,10 +5,10 @@ _start: mov rbp,rsp     ; save pointer to stack pointer
         pop rcx         ; get argument count off stack
 _nexta: pop rcx         ; loop popping off arg pointers
         cmp rcx,0       ; ... until 64-bit 0 value
-        jne _nexta
+        jne _nexta      ; GOTO!
 _nexte: pop rcx         ; loop popping off env pointers
         cmp rcx,0       ; ... until 64-bit 0 value
-        jne _nexte
+        jne _nexte      ; GOTO!
         mov rax,60      ; sys_exit syscall
         mov rdi,0       ; exit code
         syscall         ; make it so
